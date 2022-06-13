5176fc1d188b5b24b833ff07f856520d94740afc
horvathjanos0712 <horvathjanos0712@outlook.hu>
2022-06-13 13:05:35 +0200

    próba2

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -19,7 +19,7 @@ public class SauceDemoTest {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-extensions");
         //options.addArguments("--headless");
